f4b4886ed637ebdf17eb63b3d5c58643010aae61
ai-112 <147994094+slaterleonardo@users.noreply.github.com>
2024-05-27 12:07:14 -0400

    Update app.php

--- app/Config/app.php
+++ app/Config/app.php
@@ -6,6 +6,6 @@ define('SERVICES_PATH', APP_PATH . 'Services/');
 define('PUBLIC_PATH', BASE_PATH . 'public/');
 define('CONFIG_PATH', BASE_PATH . 'Config/');
 define('VIEW_PATH', BASE_PATH . 'Views/');
-define('PARTIAL_PATH', VIEW_PATH . 'Partials/');
+define('PARTIAL_PATH', VIEW_PATH . 'partials/');
 
 require_once CONFIG_PATH . 'database.php';
