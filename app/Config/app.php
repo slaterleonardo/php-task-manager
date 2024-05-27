1237dfefd517b39fbf1865e494b24bb0e7c6f075
ai-112 <147994094+slaterleonardo@users.noreply.github.com>
2024-05-27 11:42:56 -0400

    config

--- app/Config/app.php
+++ app/Config/app.php
@@ -2,9 +2,9 @@
 
 define('BASE_PATH', __DIR__ . '/../');
 define('APP_PATH', BASE_PATH . 'app/');
-define('CONFIG_PATH', BASE_PATH . 'config/');
+define('SERVICES_PATH', APP_PATH . 'Services/');
 define('PUBLIC_PATH', BASE_PATH . 'public/');
-define('SERVICES_PATH', BASE_PATH . 'Services/');
+define('CONFIG_PATH', BASE_PATH . 'Config/');
 define('VIEW_PATH', BASE_PATH . 'Views/');
 define('PARTIAL_PATH', VIEW_PATH . 'Partials/');
 
